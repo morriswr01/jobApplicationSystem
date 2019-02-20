6b5063023efb427f887ae34a3527ee5733c04f2c
morriswr01 <morriswr01@gmail.com>
2019-02-20 10:58:26 +0000

    Styling of job opening list and buttons done

--- jobApplicationProject/jobApplicationProject/settings.py
+++ jobApplicationProject/jobApplicationProject/settings.py
@@ -129,9 +129,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/'),
 ]
 
 SASS_PROCESSOR_INCLUDE_DIRS = [
