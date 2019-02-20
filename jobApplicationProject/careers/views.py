ea02a4786914a2656b4ecc086eaee704ac0ff594
morriswr01 <morriswr01@gmail.com>
2019-02-20 09:12:15 +0000

    Applicant login modal apply now button now goes to careers page

--- jobApplicationProject/careers/views.py
+++ jobApplicationProject/careers/views.py
@@ -7,4 +7,4 @@ from django.http import HttpResponse
 # Create your views here.
 def index (request):
     # return HttpResponse('Hello From Home')
-    return render(request, 'careers/index.html')
+    return render(request, 'careers/index.html',{'home': 0})
