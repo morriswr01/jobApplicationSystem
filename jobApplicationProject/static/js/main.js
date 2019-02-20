2626854659f00774d672063e0f21b1bc705a574c
morriswr01 <morriswr01@gmail.com>
2019-02-20 12:08:12 +0000

    Accordion now working for list of jobs

--- jobApplicationProject/static/js/main.js
+++ jobApplicationProject/static/js/main.js
@@ -4,19 +4,28 @@ $(document).ready(function () {
         target = $('#applicant-login-modal');
 
         target.css("display", "block");
-    })
+    });
 
     //Show admin login modal
     $('#admin-login-button').click(function () {
         target = $('#admin-login-modal');
 
         target.css("display", "block");
-    })
+    });
 
     //Close login modals on clicking cross button
     $('.close-modal').click(function () {
         target = $(this).parentsUntil('body').last();
 
         target.css("display", "none");
+    });
+
+    //Show accordion upon selecting a job to view
+    $('.selectJob').on('click', function (e) {
+        target = $(this).next().first();
+
+        $('.jobDesc').not(target).hide();
+
+        $(target).toggle('fast');
     });
 });
