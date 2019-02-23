f367347a7f8b5273a9f54f9ea16daa9a3c7b6760
morriswr01 <morriswr01@gmail.com>
2019-02-23 13:04:56 +0000

    Added more fields to your application

--- jobApplicationProject/static/js/main.js
+++ jobApplicationProject/static/js/main.js
@@ -1,4 +1,17 @@
 $(document).ready(function () {
+
+    // Remove empty fields from GET forms
+    $("form").submit(function () {
+        $(this).find(":input").filter(function () {
+            return !this.value;
+        }).attr("disabled", "disabled");
+        return true; // ensure form still submits
+    });
+
+    // Un-disable form fields when page loads, in case they click back after submission
+    $("form").find(":input").prop("disabled", false);
+
+
     //Show applicant login modal
     $('#applicant-login-button').click(function () {
         target = $('#applicant-login-modal');
