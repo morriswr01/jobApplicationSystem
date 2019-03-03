39d64953d23505d05a9acf3b1f0f2bbc45bab07a
morriswr01 <morriswr01@gmail.com>
2019-03-03 12:20:27 +0000

    Authentication check on all pages meaning being logged in always redirects you to the relevant dashboard

--- jobApplicationProject/home/views.py
+++ jobApplicationProject/home/views.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core import serializers
 from controller.models import Positions
 
 # Create your views here.
 def home (request):
-    return render(request, 'home/home.html', {'home': 1})
+    if request.user.is_authenticated:
+        return redirect('dashboard')
+    else:
+        return render(request, 'home/home.html', {'home': 1})
 
 def careers (request):
-    openPositions = serializers.serialize( "python", Positions.objects.filter(positionOpen = True) )
-    return render(request, 'home/careers.html', {'openPositions': openPositions})
+    if request.user.is_authenticated:
+        return redirect('dashboard')
+    else:    
+        openPositions = serializers.serialize( "python", Positions.objects.filter(positionOpen = True) )
+        return render(request, 'home/careers.html', {'openPositions': openPositions})
